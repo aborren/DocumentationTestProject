be70cb863596a1a3dd9b982a37dca13ea912aa9f
Dan Isacson <dan@themobilelife.com>
2016-10-12 11:17:49 +0200

    init

---
+++ DocumentationTestProject/PublicTestClass.swift
@@ -0,0 +1,37 @@
+//
+//  PublicTestClass.swift
+//  DocumentationTestProject
+//
+//  Created by Dan Isacson on 2016-10-12.
+//  Copyright © 2016 dna. All rights reserved.
+//
+
+import Foundation
+
+/**
+    # Lists
+ 
+    You can apply *italic*, **bold**, or `code` inline styles.
+ 
+    ## Unordered Lists
+ 
+    - Lists are great,
+    - but perhaps don't nest
+    - Sub-list formatting
+ 
+    - isn't the best.
+ 
+    ## Ordered Lists
+ 
+    1. Ordered lists, too
+    2. for things that are sorted;
+    3. Arabic numerals
+    4. are the only kind supported.
+ 
+ 
+ */
+public class PublicTestClass {
+    
+    
+    
+}
---
+++ DocumentationTestProject/PublicTestStruct.swift
@@ -0,0 +1,30 @@
+//
+//  PublicTestStruct.swift
+//  DocumentationTestProject
+//
+//  Created by Dan Isacson on 2016-10-12.
+//  Copyright © 2016 dna. All rights reserved.
+//
+
+import Foundation
+
+
+/// Nothing fancy to see here except the awesome emoji! 😁
+public struct PublicTestStruct {
+    
+    /**
+        The only public method in this struct.
+        - returns: An "optional" Int that's actually **always** nil.
+     */
+    func publicMethod() -> Int? {
+        return nil
+    }
+    
+    /**
+        Private and should not come out on Jazzy docs
+     */
+    private func privateMethod() {
+        print("Hi")
+    }
+    
+}
