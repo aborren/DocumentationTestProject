94faac36051afa5d5afc092b61752664cabda7a3
Dan Isacson <dan@themobilelife.com>
2016-10-12 16:24:12 +0200

    test updated

--- DocumentationTestProjectTests/DocumentationTestProjectTests.swift
+++ DocumentationTestProjectTests/DocumentationTestProjectTests.swift
@@ -23,6 +23,7 @@ class DocumentationTestProjectTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(true)
     }
     
     func testPerformanceExample() {
